11ab16b3b3241cc44dcf83c9682ae726b076e0ce
2kristint <2kristint@gmail.com>
2024-10-30 09:45:20 -0500

    course feedback pie charts

--- client/src/components/CourseSpecificReport.jsx
+++ client/src/components/CourseSpecificReport.jsx
@@ -8,7 +8,8 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  Paper
+  Paper,
+  Typography
 } from '@mui/material';
 import { PieChart, BarChart } from '@mui/x-charts';
 import Header from './layout/Header.jsx'
@@ -35,6 +36,34 @@ export default function CourseSpecificReport({ course, courseUnselect, id }) {
   const feedbackUserTypeData = resultData?.feedbackUserTypeData;
   const gradeLevelLabels = resultData?.gradeLevelLabels;
   const feedbackGradeLevelData = resultData?.feedbackGradeLevelData;
+  const Q1_OverallFeedback = resultData?.Q1_OverallFeedback;
+  const Q2_Applicable = resultData?.Q2_Applicable;
+  const Q3_EngagingAppropriate = resultData?.Q3_EngagingAppropriate;
+  const Q4_Navigate = resultData?.Q4_Navigate;
+  const Q5_Technology = resultData?.Q5_Technology;
+
+  const feedbackSurveys = [
+    {
+      label: "Overall, how satisfied or dissatisfied are you with this learning module?",
+      data: Q1_OverallFeedback
+    },
+    {
+      label: "How applicable was the content?",
+      data: Q2_Applicable
+    },
+    {
+      label: "The course material was engaging and appropriate for the topic.",
+      data: Q3_EngagingAppropriate
+    },
+    {
+      label: "How easy was it to navigate this learning module?",
+      data: Q4_Navigate
+    },
+    {
+      label: "How often did you encounter issues related to the learning module technology?",
+      data: Q5_Technology
+    }
+  ]
 
   console.log(feedbackGradeLevelData)
 
@@ -75,11 +104,57 @@ export default function CourseSpecificReport({ course, courseUnselect, id }) {
           }
 
           <Box>
-            {feedbackUserTypeData &&
+            <Typography variant="h3" gutterBottom>
+              User Types
+            </Typography>
+            <PieChart
+              series={[
+                {
+                  data: feedbackUserTypeData,
+                },
+              ]}
+              margin={{ top: 50, bottom: 50, left: 0, right: 1000 }}
+              slotProps={{
+                legend: {
+                  direction: 'column',
+                  position: { vertical: 'middle', horizontal: 'middle' },
+                  padding: 0,
+                  labelStyle: {
+                    fontSize: 14
+                  },
+                  itemMarkWidth: 11,
+                  itemMarkHeight: 10,
+                },
+              }}
+              width={1200}
+              height={300}
+            />
+          </Box>
+
+          <Box>
+            <Typography variant="h3" gutterBottom>
+              Grade Levels Worked With
+            </Typography>
+            {feedbackGradeLevelData &&
+              <BarChart
+                width={1200}
+                height={300}
+                series={[
+                  { data: feedbackGradeLevelData, id: "gradeLevel" }
+                ]}
+                xAxis={[{ data: gradeLevelLabels, scaleType: 'band' }]}
+              />}
+          </Box>
+
+          {feedbackSurveys.map((survey, index) => (
+            <Box key={index}>
+              <Typography variant="h3" gutterBottom>
+                {survey.label}
+              </Typography>
               <PieChart
                 series={[
                   {
-                    data: feedbackUserTypeData,
+                    data: survey.data,
                   },
                 ]}
                 margin={{ top: 50, bottom: 50, left: 0, right: 1000 }}
@@ -95,23 +170,11 @@ export default function CourseSpecificReport({ course, courseUnselect, id }) {
                     itemMarkHeight: 10,
                   },
                 }}
-                width={1500}
-                height={300}
-              />
-            }
-          </Box>
-
-          <Box>
-            {feedbackGradeLevelData &&
-              <BarChart
                 width={1200}
                 height={300}
-                series={[
-                  { data: feedbackGradeLevelData, id: "gradeLevel" }
-                ]}
-                xAxis={[{ data: gradeLevelLabels, scaleType: 'band' }]}
-              />}
-          </Box>
+              />
+            </Box>
+          ))}
 
           <Button variant="contained" onClick={() => courseUnselect(course.id)} sx={{ mt: 2, float: "right" }}>Close Data</Button>
         </Box>
--- server/server.js
+++ server/server.js
@@ -82,6 +82,7 @@ app.get('/getCourseData', async (req, res) => {
     const param = req.query.param;
     try {
 
+        //Number of users enrolled and completed query
         const enrollmentDataQuery = new Promise((resolve, reject) => {
             db.query(`SELECT c.fullname AS course_name, c.id,
                         CASE WHEN COUNT(*) = 0 THEN NULL ELSE COUNT(*) END AS total_users,
@@ -97,6 +98,7 @@ app.get('/getCourseData', async (req, res) => {
             });
         });
 
+        //** PreCourse survey queries **//
         const userTypeChoicesQuery = new Promise((resolve, reject) => {
             db.query(`SELECT
                         quizzes.name AS quiz,
@@ -198,31 +200,329 @@ app.get('/getCourseData', async (req, res) => {
             });
         });
 
+        //Course feedback queries
+        const Q1_OverallFeedbackChoicesQuery = new Promise((resolve, reject) => {
+            db.query(`SELECT
+                        quizzes.name AS quiz,
+                        quizzes.course,
+                        quizzes.id AS quiz_id,
+                        questions.name AS question_name,
+                        questions.id AS question_id,
+                        questions.presentation AS answer_choices
+                    FROM dbgyt2oi9llwgg.mdlxk_feedback quizzes
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_item questions
+                        ON quizzes.id = questions.feedback
+                    WHERE quizzes.course =  ${param}
+                        AND LOWER(quizzes.name) LIKE LOWER('%Course Feedback%')
+                        AND LOWER(questions.name) LIKE LOWER('%Overall, how satisfied or dissatisfied are you with this learning module?%');`, (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+
+        const Q1_OverallFeedbackAnswersQuery = new Promise((resolve, reject) => {
+            db.query(`SELECT
+                        quizzes.name AS quiz,
+                        quizzes.course,
+                        quizzes.id AS quiz_id,
+                        questions.name AS question_name,
+                        questions.id AS question_id,
+                        answers.value AS answer,
+                        COUNT(answers.value) AS answerCount
+                    FROM dbgyt2oi9llwgg.mdlxk_feedback quizzes
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_item questions
+                        ON quizzes.id = questions.feedback
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_value answers
+                        ON questions.id = answers.item
+                    WHERE quizzes.course =  ${param}
+                        AND LOWER(quizzes.name) LIKE LOWER('%Course Feedback%')
+                        AND LOWER(questions.name) LIKE LOWER('%Overall, how satisfied or dissatisfied are you with this learning module?%')
+                    GROUP BY questions.id, quizzes.id, answers.value
+                    ORDER BY quizzes.id, questions.id;`, (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+
+        const Q2_ApplicableChoicesQuery = new Promise((resolve, reject) => {
+            db.query(`SELECT
+                        quizzes.name AS quiz,
+                        quizzes.course,
+                        quizzes.id AS quiz_id,
+                        questions.name AS question_name,
+                        questions.id AS question_id,
+                        questions.presentation AS answer_choices
+                    FROM dbgyt2oi9llwgg.mdlxk_feedback quizzes
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_item questions
+                        ON quizzes.id = questions.feedback
+                    WHERE quizzes.course =  ${param}
+                        AND LOWER(quizzes.name) LIKE LOWER('%Course Feedback%')
+                        AND LOWER(questions.name) LIKE LOWER('%In your position, how applicable was the content presented in this learning module?%');`, (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+
+        const Q2_ApplicableAnswersQuery = new Promise((resolve, reject) => {
+            db.query(`SELECT
+                        quizzes.name AS quiz,
+                        quizzes.course,
+                        quizzes.id AS quiz_id,
+                        questions.name AS question_name,
+                        questions.id AS question_id,
+                        answers.value AS answer,
+                        COUNT(answers.value) AS answerCount
+                    FROM dbgyt2oi9llwgg.mdlxk_feedback quizzes
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_item questions
+                        ON quizzes.id = questions.feedback
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_value answers
+                        ON questions.id = answers.item
+                    WHERE quizzes.course =  ${param}
+                        AND LOWER(quizzes.name) LIKE LOWER('%Course Feedback%')
+                        AND LOWER(questions.name) LIKE LOWER('%Overall, how satisfied or dissatisfied are you with this learning module?%')
+                    GROUP BY questions.id, quizzes.id, answers.value
+                    ORDER BY quizzes.id, questions.id;`, (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+
+        const Q3_EngagingAppropriateChoicesQuery = new Promise((resolve, reject) => {
+            db.query(`SELECT
+                        quizzes.name AS quiz,
+                        quizzes.course,
+                        quizzes.id AS quiz_id,
+                        questions.name AS question_name,
+                        questions.id AS question_id,
+                        questions.presentation AS answer_choices
+                    FROM dbgyt2oi9llwgg.mdlxk_feedback quizzes
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_item questions
+                        ON quizzes.id = questions.feedback
+                    WHERE quizzes.course =  ${param}
+                        AND LOWER(quizzes.name) LIKE LOWER('%Course Feedback%')
+                        AND LOWER(questions.name) LIKE LOWER('%The course material was engaging and appropriate for the topic.%');`, (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+
+        const Q3_EngagingAppropriateAnswersQuery = new Promise((resolve, reject) => {
+            db.query(`SELECT
+                        quizzes.name AS quiz,
+                        quizzes.course,
+                        quizzes.id AS quiz_id,
+                        questions.name AS question_name,
+                        questions.id AS question_id,
+                        answers.value AS answer,
+                        COUNT(answers.value) AS answerCount
+                    FROM dbgyt2oi9llwgg.mdlxk_feedback quizzes
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_item questions
+                        ON quizzes.id = questions.feedback
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_value answers
+                        ON questions.id = answers.item
+                    WHERE quizzes.course =  ${param}
+                        AND LOWER(quizzes.name) LIKE LOWER('%Course Feedback%')
+                        AND LOWER(questions.name) LIKE LOWER('%The course material was engaging and appropriate for the topic.%')
+                    GROUP BY questions.id, quizzes.id, answers.value
+                    ORDER BY quizzes.id, questions.id;`, (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+
+        const Q4_NavigateChoicesQuery = new Promise((resolve, reject) => {
+            db.query(`SELECT
+                        quizzes.name AS quiz,
+                        quizzes.course,
+                        quizzes.id AS quiz_id,
+                        questions.name AS question_name,
+                        questions.id AS question_id,
+                        questions.presentation AS answer_choices
+                    FROM dbgyt2oi9llwgg.mdlxk_feedback quizzes
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_item questions
+                        ON quizzes.id = questions.feedback
+                    WHERE quizzes.course =  ${param}
+                        AND LOWER(quizzes.name) LIKE LOWER('%Course Feedback%')
+                        AND LOWER(questions.name) LIKE LOWER('%How easy was it to navigate this learning module%');`, (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+
+        const Q4_NavigateAnswersQuery = new Promise((resolve, reject) => {
+            db.query(`SELECT
+                        quizzes.name AS quiz,
+                        quizzes.course,
+                        quizzes.id AS quiz_id,
+                        questions.name AS question_name,
+                        questions.id AS question_id,
+                        answers.value AS answer,
+                        COUNT(answers.value) AS answerCount
+                    FROM dbgyt2oi9llwgg.mdlxk_feedback quizzes
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_item questions
+                        ON quizzes.id = questions.feedback
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_value answers
+                        ON questions.id = answers.item
+                    WHERE quizzes.course =  ${param}
+                        AND LOWER(quizzes.name) LIKE LOWER('%Course Feedback%')
+                        AND LOWER(questions.name) LIKE LOWER('%How easy was it to navigate this learning module%')
+                    GROUP BY questions.id, quizzes.id, answers.value
+                    ORDER BY quizzes.id, questions.id;`, (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+
+        const Q5_TechnologyChoicesQuery = new Promise((resolve, reject) => {
+            db.query(`SELECT
+                        quizzes.name AS quiz,
+                        quizzes.course,
+                        quizzes.id AS quiz_id,
+                        questions.name AS question_name,
+                        questions.id AS question_id,
+                        questions.presentation AS answer_choices
+                    FROM dbgyt2oi9llwgg.mdlxk_feedback quizzes
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_item questions
+                        ON quizzes.id = questions.feedback
+                    WHERE quizzes.course =  ${param}
+                        AND LOWER(quizzes.name) LIKE LOWER('%Course Feedback%')
+                        AND LOWER(questions.name) LIKE LOWER('%How often did you encounter issues related to the learning module technology?%');`, (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+
+        const Q5_TechnologyAnswersQuery = new Promise((resolve, reject) => {
+            db.query(`SELECT
+                        quizzes.name AS quiz,
+                        quizzes.course,
+                        quizzes.id AS quiz_id,
+                        questions.name AS question_name,
+                        questions.id AS question_id,
+                        answers.value AS answer,
+                        COUNT(answers.value) AS answerCount
+                    FROM dbgyt2oi9llwgg.mdlxk_feedback quizzes
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_item questions
+                        ON quizzes.id = questions.feedback
+                    JOIN dbgyt2oi9llwgg.mdlxk_feedback_value answers
+                        ON questions.id = answers.item
+                    WHERE quizzes.course =  ${param}
+                        AND LOWER(quizzes.name) LIKE LOWER('%Course Feedback%')
+                        AND LOWER(questions.name) LIKE LOWER('%How often did you encounter issues related to the learning module technology?%')
+                    GROUP BY questions.id, quizzes.id, answers.value
+                    ORDER BY quizzes.id, questions.id;`, (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+
         // Wait for all queries to complete
-        const [enrollmentData, userTypeChoices, userTypeAnswers, gradeLevelChoices, gradeLevelAnswers] = await Promise.all([enrollmentDataQuery, userTypeChoicesQuery, userTypeAnswersQuery, gradeLevelChoicesQuery, gradeLevelAnswersQuery]);
+        const [
+            enrollmentData,
+            userTypeChoices,
+            userTypeAnswers,
+            gradeLevelChoices,
+            gradeLevelAnswers,
+            Q1_OverallFeedbackChoices,
+            Q1_OverallFeedbackAnswers,
+            Q2_ApplicableChoices,
+            Q2_ApplicableAnswers,
+            Q3_EngagingAppropriateChoices,
+            Q3_EngagingAppropriateAnswers,
+            Q4_NavigateChoices,
+            Q4_NavigateAnswers,
+            Q5_TechnologyChoices,
+            Q5_TechnologyAnswers
+        ] = await Promise.all([
+            enrollmentDataQuery,
+            userTypeChoicesQuery,
+            userTypeAnswersQuery,
+            gradeLevelChoicesQuery,
+            gradeLevelAnswersQuery,
+            Q1_OverallFeedbackChoicesQuery,
+            Q1_OverallFeedbackAnswersQuery,
+            Q2_ApplicableChoicesQuery,
+            Q2_ApplicableAnswersQuery,
+            Q3_EngagingAppropriateChoicesQuery,
+            Q3_EngagingAppropriateAnswersQuery,
+            Q4_NavigateChoicesQuery,
+            Q4_NavigateAnswersQuery,
+            Q5_TechnologyChoicesQuery,
+            Q5_TechnologyAnswersQuery
+        ]);
 
-        //format data
+        //** format data **//
 
+        //user type pie chart
         const answerChoicesString = userTypeChoices[0].answer_choices;
         const choiceLabels = answerChoicesString.split('|'); // Splits into an array of labels          
-
         const feedbackUserTypeData = userTypeAnswers.map((ele, index) => ({
             id: index,
             value: ele.answerCount,
             label: choiceLabels[ele.answer - 1] || `Choice ${ele.answer}`
         }));
 
+        //grade level bar chart
         const gradeLevelChoicesString = gradeLevelChoices[0].answer_choices;
         const gradeLevelLabels = gradeLevelChoicesString.split('|');
-
         const feedbackGradeLevelData = gradeLevelAnswers.map(ele => ele.answerCount);
+
+        //q1 overall feedback pie chart
+        const Q1_OverallFeedbackChoicesString = Q1_OverallFeedbackChoices[0].answer_choices;
+        const Q1_OverallFeedbackLabels = Q1_OverallFeedbackChoicesString.split('|'); // Splits into an array of labels          
+        const Q1_OverallFeedback = Q1_OverallFeedbackAnswers.map((ele, index) => ({
+            id: index,
+            value: ele.answerCount,
+            label: Q1_OverallFeedbackLabels[ele.answer - 1] || `Choice ${ele.answer}`
+        }));
+
+        //q2 applicable pie chart
+        const Q2_ApplicableChoicesString = Q2_ApplicableChoices[0].answer_choices;
+        const Q2_ApplicableLabels = Q2_ApplicableChoicesString.split('|'); // Splits into an array of labels          
+        const Q2_Applicable = Q2_ApplicableAnswers.map((ele, index) => ({
+            id: index,
+            value: ele.answerCount,
+            label: Q2_ApplicableLabels[ele.answer - 1] || `Choice ${ele.answer}`
+        }));
+
+        //Q3_EngagingAppropriate pie chart
+        const Q3_EngagingAppropriateString = Q3_EngagingAppropriateChoices[0].answer_choices;
+        const Q3_EngagingAppropriateLabels = Q3_EngagingAppropriateString.split('|'); // Splits into an array of labels          
+        const Q3_EngagingAppropriate = Q3_EngagingAppropriateAnswers.map((ele, index) => ({
+            id: index,
+            value: ele.answerCount,
+            label: Q3_EngagingAppropriateLabels[ele.answer - 1] || `Choice ${ele.answer}`
+        }));
+
+        //Q4_Navigate pie chart
+        const Q4_NavigateString = Q4_NavigateChoices[0].answer_choices;
+        const Q4_NavigateLabels = Q4_NavigateString.split('|'); // Splits into an array of labels          
+        const Q4_Navigate = Q4_NavigateAnswers.map((ele, index) => ({
+            id: index,
+            value: ele.answerCount,
+            label: Q4_NavigateLabels[ele.answer - 1] || `Choice ${ele.answer}`
+        }));
+
+        //Q5_Technology pie chart
+        const Q5_TechnologyString = Q5_TechnologyChoices[0].answer_choices;
+        const Q5_TechnologyLabels = Q5_TechnologyString.split('|'); // Splits into an array of labels          
+        const Q5_Technology = Q5_TechnologyAnswers.map((ele, index) => ({
+            id: index,
+            value: ele.answerCount,
+            label: Q5_TechnologyLabels[ele.answer - 1] || `Choice ${ele.answer}`
+        }));
 
         // Send combined response as JSON
         res.json({
             enrollmentData,
             feedbackUserTypeData,
             gradeLevelLabels,
-            feedbackGradeLevelData
+            feedbackGradeLevelData,
+            Q1_OverallFeedback,
+            Q2_Applicable,
+            Q3_EngagingAppropriate,
+            Q4_Navigate,
+            Q5_Technology
         });
 
     } catch (err) {
